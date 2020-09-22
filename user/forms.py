2c574fdf7cea7c8f42747d5d960e9a6e3707ac7e
Jose Garcia <jose.ragarcia82@gmail.com>
2020-09-22 09:21:48 -0300

    Finalizado Registro de Usuários

--- user/forms.py
+++ user/forms.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .models import My_User
 
 class CreateUser(UserCreationForm):
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'controls', 'placeholder': 'Digite uma senha válida'}),
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'controls', 'placeholder': 'Repita a senha'}),
+    )
     class Meta:
         model = My_User
-        fields = ['first_name', 'last_name', 'username', 'email', 'photo']
+        fields = ['first_name', 'last_name', 'username', 'email']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'controls', 'placeholder': 'Digite seu nome'}),
+            'last_name': forms.TextInput(attrs={'class': 'controls', 'placeholder': 'Digite seu sobrenome'}),
+            'username': forms.TextInput(attrs={'class': 'controls', 'placeholder': 'Digite seu apelido'}),
+            'email': forms.TextInput(attrs={'class': 'controls', 'placeholder': 'Digite seu email'}),
+        }
