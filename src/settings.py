96ab5fbfd9c3306f1f9fea6333caf01078e299b4
Jose Garcia <jose.ragarcia82@gmail.com>
2020-10-29 12:19:33 -0300

    Alterado CSS de profile.html

--- src/settings.py
+++ src/settings.py
@@ -86,17 +86,6 @@ default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl)}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ufolabs',
-#         'USER': 'postgres',
-#         'PASSWORD': 'vai3vezes',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
